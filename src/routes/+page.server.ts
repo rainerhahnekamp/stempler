43c167763b6f5352049261f0f3673e814d2f7677
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-01-07 23:17:12 +0100

    use prisma edge

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 import type { PageServerLoad } from '../../.svelte-kit/types/src/routes/$types';
 
 export const load: PageServerLoad = async () => {
