af6b8399072a01292f6a8da04d9b3ca59d193b75
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-01-14 22:17:16 +0100

    add resumability

--- src/components/counter.svelte
+++ src/components/counter.svelte
@@ -2,22 +2,16 @@
 	import { createEventDispatcher } from 'svelte';
 	import { formatDuration } from '../date/format-duration.ts';
 
-	let status = 'stopped';
-	let start;
-	let now = undefined;
-	let timer = '';
+	export let start = new Date();
+	let now = new Date();
+	let timer = formatDuration(start, now);
 
-	let name = '';
-	let tags = '';
+	export let name = '';
+	export let tags = '';
 	let intervalId = 0;
 
-	const run = () => {
-		start = now = new Date();
-		status = 'running';
-		intervalId = setInterval(() => (now = new Date()), 1000);
-	};
+	intervalId = setInterval(() => (now = new Date()), 1000);
 	const stop = () => {
-		status = 'stopped';
 		clearInterval(intervalId);
 		dispatch('measured', {
 			name,
@@ -36,26 +30,22 @@
 	const dispatch = createEventDispatcher();
 </script>
 
-{#if status === 'stopped'}
-	<button class="button-green" on:click={run}>Start</button>
-{:else}
-	<p>{timer}</p>
-	<form>
-		<div class="flex">
-			<input
-				class="border border-gray-300 focus:outline-blue-400 rounded w-full h-8 p-3 text-sm"
-				name="name"
-				placeholder="name"
-				bind:value={name}
-			/>
-			<input
-				class="border border-gray-300 focus:outline-blue-400 rounded w-full h-8 p-3 text-sm"
-				name="tags"
-				placeholder="tags"
-				bind:value={tags}
-			/>
+<p>{timer}</p>
+<form>
+	<div class="flex">
+		<input
+			class="border border-gray-300 focus:outline-blue-400 rounded w-full h-8 p-3 text-sm"
+			name="name"
+			placeholder="name"
+			bind:value={name}
+		/>
+		<input
+			class="border border-gray-300 focus:outline-blue-400 rounded w-full h-8 p-3 text-sm"
+			name="tags"
+			placeholder="tags"
+			bind:value={tags}
+		/>
 
-			<button class="button-red" on:click={stop}>Stop</button>
-		</div>
-	</form>
-{/if}
+		<button class="button-red" on:click={stop}>Stop</button>
+	</div>
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,28 +8,41 @@
 	import { auth } from '../auth/auth.ts';
 
 	let measurements = [];
+	let status = 'stopped';
+	let name = '';
+	let tags = '';
+	const start = (measurementName = '', measurementTags = '') => {
+		name = measurementName;
+		tags = measurementTags;
+		status = 'running';
+	};
 	const measurementService = new MeasurementService();
 	const sorter = (m1, m2) => m2.start - m1.start;
 
 	measurementService.findAll().then((records) => (measurements = records.sort(sorter)));
 
 	const saveMeasurement = async (event) => {
+		status = 'stopped';
 		const entry = measurementSchema.parse({ id: 0, ...event.detail });
 		const measurement = await measurementService.saveMeasurement(entry);
 		measurements = [...measurements, measurement].sort(sorter);
 	};
 
 	const remove = async (id) => {
-		measurements = [...(await measurementService.remove(id))];
+		measurements = [...(await measurementService.remove(id)).sort(sorter)];
 	};
 
-	const resume = async () => {
-		alert('not yet implemented');
+	const resume = async (measurement) => {
+		start(measurement.name, measurement.tags.join(', '));
 	};
 </script>
 
 {#if $auth.username}
-	<Counter on:measured={saveMeasurement} />
+	{#if status === 'running'}
+		<Counter on:measured={saveMeasurement} {name} {tags} />
+	{:else}
+		<button class="button-green" on:click={() => start()}>Start</button>
+	{/if}
 
 	{#if measurements.length}
 		<h2 class="text-2xl font-bold mt-4 mb-2">Measurements</h2>
@@ -49,7 +62,7 @@
 				<p>{formatDuration(measurement.start, measurement.end)}</p>
 				<div>
 					<button class="button-red" on:click={() => remove(measurement.id)}>Delete</button>
-					<button class="button" on:click={() => resume(measurement.id)}>Resume</button>
+					<button class="button" on:click={() => resume(measurement)}>Resume</button>
 				</div>
 			{/each}
 		</div>
